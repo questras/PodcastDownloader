11efd3a14c6f6f59bac8fbb906cd15fd9a4e9c17
questras <michu123qwe@gmail.com>
2020-06-20 16:09:38 +0200

    add printing downloading info

--- PodcastDownloader/downloader.py
+++ PodcastDownloader/downloader.py
@@ -5,7 +5,7 @@ from typing import List
 class Downloader:
     """Class to download and save podcasts."""
 
-    def download_podcast(url: str, filepath: str):
+    def download_podcast(self, url: str, filepath: str):
         """Download podcast in given url and save it to 
         given filepath."""
 
@@ -13,10 +13,22 @@ class Downloader:
 
         with open(filepath, 'wb') as file:
             file.write(r.content)
+
+    def print_info(self, url: str, filepath: str):
+        """Print downloading info."""
+
+        print(f'Now downloading: \n{url}')
+        print(f'Save in: \n{filepath}')
+        print()
     
-    def download_podcasts(urls_and_filepaths: List[tuple]):
+    def download_podcasts(self, urls_and_filepaths: List[tuple], 
+                          log: bool=False):
         """Download podcasts in given urls and save them to 
-        corresponding filepaths."""
+        corresponding filepaths. Info about downloading is printed
+        if log is set to True"""
 
         for url, filepath in urls_and_filepaths:
+            if log:
+                self.print_info()
+
             download_podcast(url, filepath)
