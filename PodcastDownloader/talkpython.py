b762419f8fda8e76bee6fd17c804b41c49697474
questras <michu123qwe@gmail.com>
2020-06-20 16:09:02 +0200

    add module to get Talk Python podcast download data

---
+++ PodcastDownloader/talkpython.py
@@ -0,0 +1,30 @@
+import bs4
+import requests
+from typing import List
+
+class TalkPythonPodcast:
+    """Class to get urls and names of all episodes
+    of Talk Python podcast."""
+
+    def get_urls_and_names(self) -> List[tuple]:
+        """Return  urls and names of all episodes
+        of Talk Python podcast"""
+
+        episodes_url = 'https://talkpython.fm/episodes/all'
+        urlbase = 'https://talkpython.fm'
+
+        r = requests.get(episodes_url)
+        r.raise_for_status()
+
+        soup = bs4.BeautifulSoup(r.text, features="html.parser")
+        a_elements = soup.findAll('a')
+
+        urls_and_names = []
+        for element in a_elements:
+            if 'episodes/show' in element['href']:
+                url = urlbase + element['href'].replace('/show/', '/download/')
+                name = url.split('/')[-1] + '.mp3'
+
+                urls_and_names.append((url, name))
+        
+        return urls_and_names
